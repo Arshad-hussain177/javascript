f3b2f4c03df39b177570b64bdbcc472b68946e5a
Arshad-hussain177 <arshadhussain1729@gmail.com>
2025-06-18 16:03:56 +0000

    Conversion of datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let score = "33abc"
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof(valueInNumber));
+console.log(valueInNumber);
+
+// "33"=>33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLog =1
+let boollog = Boolean(isLog)
+console.log(boollog);
+
+//1=> true; 0=> false
+// ""=>false
+// "hitesh" => true
+
+let someN = 33
+
+let stringN = String(someN)
+console.log(stringN);
