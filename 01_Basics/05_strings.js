c0d866a1c69b9f3817fc089c7ee43340095fafdb
Arshad-hussain177 <arshadhussain1729@gmail.com>
2025-06-19 17:43:39 +0000

    Strings

---
+++ 01_Basics/05_strings.js
@@ -0,0 +1,19 @@
+const name = 'Arshad'
+const age = 21
+
+//console.log(name+age)
+
+//String Interpolation:- To provide placeholders in order to display different values
+console.log(`Hello my name is ${name} and my age is ${age}.`);
+
+
+//Another way of String declaration
+const gameName = new String('Arshadfc')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+console.log(gameName.length);
+
+
