f53d82645af791500bdd684b823bfcdf1987606a
Arshad-hussain177 <arshadhussain1729@gmail.com>
2025-06-19 17:59:49 +0000

    Strings

--- 01_Basics/05_strings.js
+++ 01_Basics/05_strings.js
@@ -16,4 +16,21 @@ console.log(gameName.__proto__);
 
 console.log(gameName.length);
 
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(3));
+console.log(gameName.indexOf('d'));
 
+const newString = gameName.substring(0,5);
+console.log(newString);
+
+const anotherString = gameName.slice(-7,4);
+console.log(anotherString);
+
+const Stringone = "    Arshad    "
+console.log(Stringone);
+console.log(Stringone.trim());
+
+const url = "https://arshad.com/arshad%20hussain"
+console.log(url.replace('%20',"_"));
+
+console.log(url.includes('Arshad'));
