b5bb72ff7220626e49b9ebd8c555a9dc5a5a9e8d
Arshad-hussain177 <arshadhussain1729@gmail.com>
2025-09-06 18:42:40 +0000

    Math and Num

---
+++ 01_Basics/06_nums_and_maths.js
@@ -0,0 +1,39 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+const otherNumber = 575.87545
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// -------------------Maths-----------------
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.7));
+console.log(Math.ceil(4.2));
+console.log(Math.floor(4.9));
+console.log(Math.min(4,3,9,5));
+console.log(Math.max(5,9,7,6));
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random() * (max-min+1))+min);
+// used floor inorder to get the lower single value and add the min oinorder to make sure that the min possible value is achieved.
+
+
+
+
+
+
+
